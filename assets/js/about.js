2ec827c71d0d1ecd5c1ee139fbc45c9be08d37a9
Nevyana <nevy.angelova@gmail.com>
2016-01-14 00:23:01 +0200

    Template
    Custom written in code editor

---
+++ assets/js/about.js
@@ -0,0 +1,44 @@
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 980px)',
+		narrower: '(max-width: 840px)',
+		mobile: '(max-width: 736px)',
+		mobilep: '(max-width: 480px)'
+	});
+    
+    $(document).ready(function() {
+        $('#menu').click(function() {
+            $('#menudown').toggle(0);
+        });
+        });
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+
+		// Fix: Placeholder.
+			$('form').placeholder();
+
+		// Prioritize "important" elements.
+			skel.on('+narrower -narrower', function() {
+				$.prioritize(
+					'.important\\28 narrower\\29',
+					skel.breakpoint('narrower').active
+				);
+			});
+
+	});
+
+})(jQuery);
